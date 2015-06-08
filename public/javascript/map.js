228e1566291d61ffdba7a0d41899dc7c84896bae
may ang <mayday815@gmail.com>
2015-06-07 21:56:15 -0700

    Tooltips
    Should redo natively.

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,15 +1,35 @@
 var s = Snap();
 
-Snap.load(config.map, function(f) {
-  // add to DOM!
-  g = f.selectAll('g');
-  s.append(g);
+$.ajax({
+  type: 'GET',
+  url: '/chart'
+}).then(function(chart) {
 
-  // add fill colors
-  s.selectAll('path, polygon, rect').attr({fill: config.clear}).forEach( function(e) {
-    e.hover(
-      function() { e.attr({ fill: config.fill }); },
-      function() { e.attr({ fill: config.clear }); }
-    );
+  Snap.load(config.map, function(f) {
+    // add to DOM!
+    g = f.selectAll('g');
+    s.append(g);
+
+    // add fill colors
+    s.selectAll('path, polygon, rect').attr({fill: config.clear}).forEach( function(e) {
+      e.hover(
+        function() { e.attr({ fill: config.fill }); },
+        function() { e.attr({ fill: config.clear }); }
+      );
+    });
+
+    // add tooltips
+    $('path, svg polygon, svg rect').parent().tipsy({
+      gravity: 'w',
+      html: true,
+      title: function() {
+        var id = $(this).attr('id');
+        id = id[3] + id.substring(5);
+        return chart[id] &&chart[id]['name'] ? chart[id]['name'] : id;
+      }
+    });
+
   });
-});
+
+
+})
